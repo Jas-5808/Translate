5d934cc2bb744a23a764c468143e2432524046ad
Jas-5808 <jasursadicov2@gmail.com>
2024-11-10 13:22:11 +0500

    edit left panel and icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,7 @@ function App() {
             <Outlet/>
           </div>
           <Footer/>
+          
         </div>
       </div>
     </>
--- client/src/components/Leftside.tsx
+++ client/src/components/Leftside.tsx
@@ -1,17 +1,42 @@
 import cn from "./mainCss.module.css"
+import logo from '../assets/logo.webp'
 
 export function Leftside(){
 
     return (
         <>
-            <div className={cn.leftside}>
+            {/* <div className={cn.leftside}>
                 <div className={cn.leftside_content}>
-                    <a href="/" className={cn.logo}>Free Translate Hub</a>
+                    
+                    <a href="/" className={cn.logo_text}><img src={logo} className={cn.logo} alt="logo" />Free Translate Hub</a>
+                    
 
                     <ul className={cn.menu}>
                         <li><a href="/">Переводчик</a></li>
                         <li><a href="/colorPicker">Color Picker</a></li>
                         <li><a href="/about">О нас</a></li>
+                    </ul>
+                </div>
+            </div> */}
+            <div className={cn.leftside}>
+                <div className={cn.leftside_content}>
+                    {/* Логотип и название */}
+                    <a href="/" className={cn.logo_text}>
+                        <img src={logo} className={cn.logo} alt="logo" />
+                        Free Translate Hub
+                    </a>
+
+                    {/* Боковое меню */}
+                    <ul className={cn.menu}>
+                        <li className={cn.menuItem}>
+                            <a href="/" className={cn.menuLink}>Переводчик</a>
+                        </li>
+                        <li className={cn.menuItem}>
+                            <a href="/colorPicker" className={cn.menuLink}>Color Picker</a>
+                        </li>
+                        <li className={cn.menuItem}>
+                            <a href="/about" className={cn.menuLink}>О нас</a>
+                        </li>
                     </ul>
                 </div>
             </div>
